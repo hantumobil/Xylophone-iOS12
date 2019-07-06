92ee183ce6f7d7547e3c0d0e3eaf3a57b810a255
windo <herwindo.artono@gmail.com>
2019-07-06 07:49:47 +0700

    adding sound to each key

--- Xylophone/ VIewController.swift
+++ Xylophone/ VIewController.swift
@@ -7,23 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate {
     
-
+    var audioPlayer : AVAudioPlayer!
+    var selectedSoundFilename : String = ""
+    let soundArray : Array = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
-
+    
     @IBAction func notePressed(_ sender: UIButton) {
-        
-        
-        
+        selectedSoundFilename = soundArray[sender.tag - 1]
+        print(selectedSoundFilename)
+        playSound()
     }
     
-  
-
+    func playSound() {
+        let soundURL = Bundle.main.url(forResource: selectedSoundFilename, withExtension: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        }
+        catch {
+            print(error)
+        }
+        
+        audioPlayer.play()
+    }
+    
 }
 
